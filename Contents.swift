bbedd9b729de1bb8bb50611efcd008fa3052774c
y.shigeyama <shigeyama.yoichiro@gmal.com>
2021-11-23 19:46:09 +0900

    配列からfor-inループで、バス停名称、緯度経度などを順番に全て取り出すことができた。

---
+++ Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+let busStopList = [
+    "busStop010": ["name": "東京", "lat": 35.6813, "lon": 139.7671], // key : バス停名称,緯度,経度
+    "busStop020": ["name": "有楽町", "lat": 35.6749, "lon": 139.7629],
+    "busStop030": ["name": "新橋", "lat": 35.6664, "lon": 139.7583]
+]
+
+var lat1 = 0.0
+var lon1 = 0.0
+
+for aaa in busStopList.keys{
+  //  print("01")
+    print("key：\(aaa)")
+    print("value：\(busStopList[aaa]!)")
+    print("name：\(busStopList[aaa]!["name"]!)")
+    lat1 = busStopList[aaa]!["lat"]as! Double
+    lon1 = busStopList[aaa]!["lon"]as! Double
+    print("lat：\(lat1)")
+    print("lon：\(lon1)")
+}
